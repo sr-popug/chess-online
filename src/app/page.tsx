5259b76f2753db193ed59dda5fcb74b09fba5039
sr-popug <antonpower2008@gmail.com>
2024-11-30 20:24:29 +0300

    Соединил с базой данных

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import { Button } from '@/shared/ui/button'
+import { prisma } from "@/shared/lib/db";
+import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+
+  console.log(games);
+
   return (
-    <div className=''>
-      <Button size={'lg'} variant={'destructive'}>
+    <div className="">
+      <Button size={"lg"} variant={"destructive"}>
         Button
       </Button>
+      {games.map((game) => (
+        <Card className="w-90" key={game.id}>
+          <CardTitle>{game.name}</CardTitle>
+        </Card>
+      ))}
     </div>
-  )
+  );
 }
